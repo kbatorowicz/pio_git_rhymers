6e33f30bd663cae430187c5b22c0e3a0e87e46ae
Kamil Batorowicz <251124@edu.p.lodz.pl>
2025-04-04 12:26:16 +0200

    15. Dokonano walidacji projektu testami jednostkowymi

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,6 +3,7 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+// projekt poprawny, uruchomione testy przebiegly pomyslnie
 class RhymersDemo {
 
     private static final int MAX_COUNT = 15;
